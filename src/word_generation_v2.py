d603c7b62f1e6924e3b0ea35975c76a737b30295
Garri Proshian <proshian.itmo@gmail.com>
2023-11-10 21:28:37 +0300

    predict_greedy_raw_multiproc works

--- src/word_generation_v2.py
+++ src/word_generation_v2.py
@@ -7,38 +7,39 @@ import time
 
 from tqdm import tqdm
 
-def dummy_test_predict_example(i_and_data):
-    time.sleep(0.5)
-    i, data = i_and_data
-    return i, data
+# def dummy_test_predict_example(i_and_data):
+#     time.sleep(0.5)
+#     i, data = i_and_data
+#     return i, data
 
-def dummy_test_get_model_predictions(dataset):
-    """
-    Creates submission file generating words greedily.
+# def dummy_test_get_model_predictions(dataset):
+#     """
+#     Creates submission file generating words greedily.
 
-    If prediction is not in the vocabulary 
-    """
-    NUM_WORKERS = 2
-    predictions = [None] * len(dataset)
+#     If prediction is not in the vocabulary 
+#     """
+#     NUM_WORKERS = 2
+#     predictions = [None] * len(dataset)
         
-    with ProcessPoolExecutor(NUM_WORKERS) as executor:
-        process_function = dummy_test_predict_example
-        for idx, result in tqdm(executor.map(process_function, enumerate(dataset)), total=len(dataset)):
-                predictions[idx] = result
-    return predictions
+#     with ProcessPoolExecutor(NUM_WORKERS) as executor:
+#         process_function = dummy_test_predict_example
+#         for idx, result in tqdm(executor.map(process_function, enumerate(dataset)), total=len(dataset)):
+#                 predictions[idx] = result
+#     return predictions
 
 
-from typing import List
+# from typing import List
 
 
-def process_data(data: Tuple[int, Tuple[Tensor, Tensor, Tensor], str, Dict[str, GreedyGenerator]]) -> Tuple[int, str]:
+def process_example(data: Tuple[int, Tuple[Tensor, Tensor, Tensor], str, Dict[str, GreedyGenerator]]) -> Tuple[int, str]:
     i, gen_in, grid_name, grid_name_to_word_generator = data
     pred = grid_name_to_word_generator[grid_name](*gen_in)
     pred = pred.removeprefix("<sos>")
     return i, pred
 
 def predict_greedy_raw_multiproc(dataset,
-                                 grid_name_to_greedy_generator
+                                 grid_name_to_greedy_generator,
+                                 num_workers=2
                                 ) -> List[List[str]]:
     """
     Creates predictions using greedy generation.
@@ -51,11 +52,12 @@ def predict_greedy_raw_multiproc(dataset,
     """
     preds = [None] * len(dataset)
 
+    data = [(i, (xyt, kb_tokens, traj_pad_mask), grid_name, grid_name_to_greedy_generator)
+            for i, ((xyt, kb_tokens, _, traj_pad_mask, _), _, grid_name) in enumerate(dataset)]
+
     with ProcessPoolExecutor() as executor:
-        for i, pred in tqdm(executor.map(process_data, [(i, (xyt, kb_tokens, traj_pad_mask), grid_name, grid_name_to_greedy_generator) for i, ((xyt, kb_tokens, _, traj_pad_mask, _), _, grid_name) in enumerate(dataset)]), total=len(dataset)):
+        for i, pred in tqdm(executor.map(process_example, data), total=len(dataset)):
             preds[i] = [pred]
-
-
 
     return preds
     
