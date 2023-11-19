e9e39da1d7f4dbf584aef22f05dace20b8fd46ce
Garri Proshian <proshian.itmo@gmail.com>
2023-11-20 02:02:04 +0300

    Reliable orig data download, tqdm; Downloaders dir

---
+++ src/downloaders/download_original_data.py
@@ -0,0 +1,42 @@
+import requests
+from urllib.parse import urlencode
+from io import BytesIO
+from zipfile import ZipFile
+from tqdm import tqdm
+
+
+UNZIP_PATH = './data/data'
+CHUNK_SIZE = 4096
+
+
+def download_file(url, block_size=1024):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # Raise an HTTPError for bad responses
+    total_size = int(response.headers.get('content-length', 0))
+    
+    with tqdm(total=total_size, unit='iB', unit_scale=True) as progress_bar, BytesIO() as buffer:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            buffer.write(data)
+    
+    return buffer
+
+
+def main():
+    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+    public_key = 'https://disk.yandex.ru/d/IYiSpLob-zAxqg'  # link to file in Yandex.Disk
+    final_url = base_url + urlencode(dict(public_key=public_key))
+
+    try:
+        response = requests.get(final_url)
+        response.raise_for_status()
+        download_url = response.json()['href']
+        download_data = download_file(download_url, CHUNK_SIZE)
+        with ZipFile(download_data) as z:
+            z.extractall(UNZIP_PATH)
+    except requests.RequestException as e:
+        print(f"Error downloading or extracting the file: {e}")
+
+
+if __name__ == '__main__':
+    main()
