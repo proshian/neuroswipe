6814bc8853f1dc46bb892635417bdd327cee5e37
Garri Proshian <proshian.itmo@gmail.com>
2023-11-19 16:24:14 +0300

    Add unziping to download_original_data.py

--- src/download_original_data.py
+++ src/download_original_data.py
@@ -1,17 +1,24 @@
 import requests
 from urllib.parse import urlencode
-
-base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
-# public_key = 'https://yadi.sk/d/UJ8VMK2Y6bJH7A'  # Сюда вписываете вашу ссылку
-public_key = 'https://disk.yandex.ru/d/IYiSpLob-zAxqg'
+from io import BytesIO
+from zipfile import ZipFile
 
 
-# Получаем загрузочную ссылку
-final_url = base_url + urlencode(dict(public_key=public_key))
-response = requests.get(final_url)
-download_url = response.json()['href']
+UNZIP_PATH = './data/data'
 
-# Загружаем файл и сохраняем его
-download_response = requests.get(download_url)
-with open('downloaded_file.txt', 'wb') as f:   # Здесь укажите нужный путь к файлу
-    f.write(download_response.content)
+if __name__ == '__main__':
+    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+    # public_key = 'https://yadi.sk/d/UJ8VMK2Y6bJH7A'  # Сюда вписываете вашу ссылку
+    public_key = 'https://disk.yandex.ru/d/IYiSpLob-zAxqg'
+
+
+    # Получаем загрузочную ссылку
+    final_url = base_url + urlencode(dict(public_key=public_key))
+    response = requests.get(final_url)
+    download_url = response.json()['href']
+
+    # Загружаем файл
+    download_response = requests.get(download_url)
+
+    z = ZipFile(BytesIO(download_response.content))
+    z.extractall(UNZIP_PATH)
