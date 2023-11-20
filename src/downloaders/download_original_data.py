0032671414ae5696958198d3d4ddc02932fe75b4
Garri Proshian <proshian.itmo@gmail.com>
2023-11-20 14:21:28 +0300

    BytesIO outside down and ext in download_orig_data

--- src/downloaders/download_original_data.py
+++ src/downloaders/download_original_data.py
@@ -9,23 +9,24 @@ UNZIP_PATH = './data/data'
 CHUNK_SIZE = 4096
 
 
-#! It would be better to make separate download and extrat functions.
-# This can be done by writing downloaded content into a temporary file.
-def download_and_extract_file(url: str, block_size: int = 1024) -> None:
+def download(url: str, buffer: BytesIO, block_size: int = 1024) -> None:
     response = requests.get(url, stream=True)
     response.raise_for_status()  # Raise an HTTPError for bad responses
     total_size = int(response.headers.get('content-length', 0))
     
-    with tqdm(total=total_size, unit='iB', unit_scale=True, desc='Downloading') as progress_bar, BytesIO() as buffer:
+    with tqdm(total=total_size, unit='iB', unit_scale=True, desc='Downloading') as progress_bar:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
             buffer.write(data)
 
-        with ZipFile(buffer) as zf:
-            for member in tqdm(zf.infolist(), desc='Extracting'):
-                zf.extract(member, UNZIP_PATH)
+    buffer.seek(0)
+
+
+def extract(buffer: BytesIO, unzip_path: str):
+    with ZipFile(buffer) as zf:
+        for member in tqdm(zf.infolist(), desc='Extracting'):
+            zf.extract(member, unzip_path)
             
-        
 
 def main():
     base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
@@ -36,7 +37,10 @@ def main():
         response = requests.get(final_url)
         response.raise_for_status()
         download_url = response.json()['href']
-        download_and_extract_file(download_url, CHUNK_SIZE)
+
+        with BytesIO() as buffer:
+            download(download_url, buffer, CHUNK_SIZE)
+            extract(buffer, UNZIP_PATH)
         
     except requests.RequestException as e:
         print(f"Error downloading or extracting the file: {e}")
