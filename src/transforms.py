e5c5555d0e270b55b4766e42df5bf9eab5f27ffc
Garri Proshian <proshian.itmo@gmail.com>
2024-03-03 22:34:36 +0300

    add EncoderFeaturesGetter

--- src/transforms.py
+++ src/transforms.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor
 
 from typing import Any, Tuple, Dict, Optional, Iterable, Callable
 from array import array
@@ -47,8 +48,36 @@ class ComposeTuple:
         return result
 
 
-def det_dx_dt(x, t):
-    pass
+def get_dx_dt(X: Tensor,
+              T: Tensor) -> Tensor:
+    """
+    Calculates dx/dt for a list of x coordinates and a list of t coordinates.
+
+    Arguments:
+    ----------
+    X : Tensor
+        x (position) coordinates.
+    T : Tensor
+        T[i] = time (ms) from swipe start corresponding to X[i].
+    """
+    # X, T = (Tensor(arr) for arr in (X, T))
+    dx_dt = torch.zeros_like(X)
+    # dx_dt[1:-1] = (X[2:] - X[:-2]) / (T[2:] - T[:-2])
+    dx_dt[1:len(X)-1] = (X[2:len(X)] - X[:len(X)-2]) / (T[2:len(X)] - T[:len(X)-2])
+
+    # Example:
+    # x0 x1 x2 x3
+    # t0 t1 t2 t3
+    # dx_dt[0] = 0
+    # dx_dt[1] = (x2 - x0) / (t2 - t0)
+    # dx_dt[2] = (x3 - x1) / (t3 - t1)
+    # dx_dt[3] = 0
+
+    # if True in torch.isnan(dx_dt):
+    #     print(dx_dt)
+    #     raise ValueError("dx_dt contains NaNs")
+
+    return dx_dt
 
 
 class DerivativesAugmentor:
@@ -63,32 +92,55 @@ class DerivativesAugmentor:
 
 
 
-class FullFeaturesGetter:
+class EncoderFeaturesGetter:
     def __init__(self, 
                  grid_name_to_nk_lookup: Dict[str, NearestKeyLookup],
-                 kb_tokenizer: KeyboardTokenizerv1
+                 kb_tokenizer: KeyboardTokenizerv1,
+                 include_time: bool,
+                 include_velocities: bool,
+                 include_accelerations: bool
                  ) -> None:
         self.grid_name_to_nk_lookup = grid_name_to_nk_lookup
         self.kb_tokenizer = kb_tokenizer
+        self.include_time = include_time
+        self.include_velocities = include_velocities
+        self.include_accelerations = include_accelerations
     
-    def _get_kb_tokens(self, X, Y, grid_name):
+    def _get_kb_tokens(self, X, Y, grid_name) -> Tensor:
         nearest_key_lookup = self.grid_name_to_nk_lookup[grid_name]
         kb_labels = [nearest_key_lookup(x, y) for x, y in zip(X, Y)]
         kb_tokens = [self.kb_tokenizer.get_token(label) for label in kb_labels]
-        kb_tokens = torch.tensor(kb_tokens, dtype=torch.int64)
+        kb_tokens = Tensor(kb_tokens, dtype=torch.int64)
         return kb_tokens
+    
+    def _get_traj_feats(self, X: Tensor, Y: Tensor, T: Tensor) -> Tensor:
+        traj_feats = [X, Y]
+        if self.include_time:
+            traj_feats.append(T)
+
+        if self.include_velocities:
+            dx_dt = get_dx_dt(X, T)
+            dy_dt = get_dx_dt(Y, T)
+            traj_feats.extend([dx_dt, dy_dt])
+        
+        if self.include_accelerations:
+            d2x_dt2 = get_dx_dt(dx_dt, T)
+            d2y_dt2 = get_dx_dt(dy_dt, T)
+            traj_feats.extend([d2x_dt2, d2y_dt2])
+        
+        traj_feats = torch.cat(
+            traj_feats,
+            axis = 1
+        )
+
+        return traj_feats
+
 
     def __call__(self, input_data):
         X, Y, T, grid_name = input_data
-        kb_tokens = self._get_kb_tokens(X, Y, T)
-        
+        X, Y, T = (Tensor(arr) for arr in (X, Y, T))
+        kb_tokens = self._get_kb_tokens(X, Y, grid_name)
+        traj_feats = self._get_traj_feats(X, Y, T)
+        return kb_tokens, traj_feats
 
-
-
-
-
-
-
-
-        
 
