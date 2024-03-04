aea3e819b498fe74051a0c26467f06ecb454083b
Garri Proshian <proshian.itmo@gmail.com>
2024-03-04 12:08:27 +0300

    Add ExtendedNearestKeyLookup

--- src/nearest_key_lookup.py
+++ src/nearest_key_lookup.py
@@ -1,11 +1,13 @@
 import pickle
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Iterable
 
 import numpy as np
 
 
 class NearestKeyLookup:
-    def __init__(self, grid, nearest_key_candidates):
+    def __init__(self, 
+                 grid: dict, 
+                 nearest_key_candidates: Iterable[str]) -> None:
         self._nearest_key_candidates = nearest_key_candidates
         self.grid = grid
         self.coord_to_kb_label = self._create_coord_to_kb_label(grid)
@@ -53,7 +55,6 @@ class NearestKeyLookup:
                 nearest_kb_label = label 
         return nearest_kb_label
     
-
     def _create_coord_to_kb_label(self, grid: dict) -> np.array: # dtype = object
         coord_to_kb_label = np.zeros(
             (grid['width'], grid['height']), dtype=object)  # 1080 x 640 in our case
@@ -93,13 +94,17 @@ class NearestKeyLookup:
         if x < 0 or x >= self.grid['width'] or y < 0 or y >= self.grid['height']:
             return self._get_kb_label_without_map(x, y, self.grid)
         return self.coord_to_kb_label[x, y]
-        
-    def save_state(self, path: str) -> None:
+    
+    def _get_state(self) -> dict:
         state = {
             'nearest_key_candidates': self._nearest_key_candidates,
             'coord_to_kb_label': self.coord_to_kb_label,
             'grid': self.grid
         }
+        return state
+        
+    def save_state(self, path: str) -> None:
+        state = self._get_state()
         with open(path, 'wb') as f:
             pickle.dump(state, f)
         
@@ -114,3 +119,47 @@ class NearestKeyLookup:
         obj.coord_to_kb_label = state['coord_to_kb_label']
 
         return obj
+
+
+class ExtendedNearestKeyLookup(NearestKeyLookup):
+    """
+    In addition to the NearestKeyLookup, this class also stores
+    the nearest key labels for the coordinates given in the extended_coords
+    list. 
+    This is useful during training when all the out-of-bounds
+    coordinates are known and we can precompute the nearest key labels. 
+    """
+    def __init__(self, 
+                 grid: dict, 
+                 nearest_key_candidates: Iterable[str],
+                 extended_coords: Iterable[Tuple[int, int]]) -> None:
+        self._nearest_key_candidates = nearest_key_candidates
+        self.grid = grid
+        self.coord_to_kb_label = self._create_coord_to_kb_label(grid)
+        self.extended_coord_to_kb_label = {
+            (x,y): self._get_kb_label_without_map(x, y) 
+            for x, y in extended_coords}
+
+    def get_nearest_kb_label(self, x, y):
+        if (x, y) in self.extended_coord_to_kb_label:
+            return self.extended_coord_to_kb_label[(x, y)]
+        return super().get_nearest_kb_label(x, y)
+    
+    def _get_state(self) -> dict:
+        state = super()._get_state()
+        state['extended_coord_to_kb_label'] = self.extended_coord_to_kb_label
+        return state
+    
+    @classmethod
+    def load_state(cls, path: str):
+        with open(path, 'rb') as f:
+            state = pickle.load(f)
+        obj = cls.__new__(cls)
+
+        obj._nearest_key_candidates = state['nearest_key_candidates']
+        obj.grid = state['grid']
+        obj.coord_to_kb_label = state['coord_to_kb_label']
+        obj.extended_coord_to_kb_label = state['extended_coord_to_kb_label']
+
+        return obj
+    
