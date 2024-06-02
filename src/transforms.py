d3adc5a274e597cd8fc4df8d49126f9e0ef00323
Garri Proshian <proshian.itmo@gmail.com>
2024-06-02 20:04:36 +0300

    Add normalized_weight_function

--- src/transforms.py
+++ src/transforms.py
@@ -149,6 +149,12 @@ class DistancesGetter:
 
 def weights_function_v1(distances: Tensor, half_key_diag, bias = 4, scale = 1.8) -> Tensor:
     """
+    Arguments:
+    ----------
+    distances: Tensor
+        A 2d tensor where i-th element is a vector of distances 
+        for i-th swipe dot and each of keyboard keys.
+
     $$f(x) = \frac{1}{1+e^{\frac{s \cdot x}{key\_radius} - b}}$$
     b = bias = 4
     s = scale = 1.8
@@ -172,6 +178,36 @@ def weights_function_v1_softmax(distances: Tensor, half_key_diag, bias = 4, scal
     # -inf to zero out unpresent values and have a sum of one 
     weights.masked_fill_(mask, float('-inf'))
     return torch.nn.functional.softmax(weights, dim=1)
+
+
+
+def weights_function_sigmoid_normalized_v1(distances: Tensor, 
+                                           half_key_diag,
+                                           bias = 4, scale = 1.8) -> Tensor:
+    """
+    Arguments:
+    ----------
+    distances: Tensor
+        A 2d tensor where i-th element is a vector of distances 
+        for i-th swipe dot and each of keyboard keys.
+
+    $$f(x) = \frac{1}{1+e^{\frac{s \cdot x}{key\_radius} - b}}$$
+    b = bias = 4
+    s = scale = 1.8
+    """
+    
+    # return 1 / (1 + torch.exp(1.8 * distances - 4))
+
+    # Sqrt beacuse currently distances is squared euclidian distance
+
+    #! It may be a good idea to move division by half_key_diag outside
+    # this function.  The division is just a scaling of distances
+    # so that they are not in pixels but use half_key_diag as a unit. 
+    sigmoid_input = distances.sqrt() / half_key_diag * (-scale) + bias
+    sigmoidal_weights = torch.nn.functional.sigmoid(sigmoid_input)
+    weights = sigmoidal_weights / sigmoidal_weights.sum(dim=1, keepdim=True)
+    return weights
+
 
 
 class KeyWeightsGetter:
