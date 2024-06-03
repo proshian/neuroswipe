0d0ca89da5148f4e1aa2f45d11d5f0f1cf7deb7f
Garri Proshian <proshian.itmo@gmail.com>
2024-06-03 03:00:21 +0300

    Add v3 models

--- src/model.py
+++ src/model.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SwipeCurveTransformerEncoderv1(nn.Module):
     """
     Transformer-based Curve encoder takes in a sequence of vectors and creates a representation
@@ -1394,6 +1395,222 @@ def get_transformer_bigger_nearest__v2(device = None,
 
 
 
+################################################################################
+##################                 V3 models                  ##################
+################################################################################
+
+
+class EncoderDecoderTransformerLike(nn.Module):
+    def __init__(self, 
+                 enc_in_emb_model: nn.Module, 
+                 dec_in_emb_model: nn.Module, 
+                 encoder: nn.Module, 
+                 decoder: nn.Module, 
+                 out: nn.Module):
+        super().__init__()
+        self.enc_in_emb_model = enc_in_emb_model
+        self.dec_in_emb_model = dec_in_emb_model
+        self.encoder = encoder
+        self.decoder = decoder
+        self.out = out  # linear
+
+    # x can be a tuple (ex. traj_feats, kb_tokens) or a single tensor
+    # (ex. just kb_tokens).
+    def encode(self, x, x_pad_mask):
+        x = self.enc_in_emb_model(x)
+        return self.encoder(x, src_key_padding_mask = x_pad_mask)
+    
+    def decode(self, y, x_encoded, memory_key_padding_mask, tgt_key_padding_mask):
+        y = self.dec_in_emb_model(y)
+        tgt_mask = self._get_mask(len(y)).to(device=self.device)
+        dec_out = self.decoder(y, x_encoded, tgt_mask=tgt_mask, 
+                               memory_key_padding_mask=memory_key_padding_mask, 
+                               tgt_key_padding_mask=tgt_key_padding_mask)
+        return self.out(dec_out)
+
+    def forward(self, x, y, x_pad_mask, y_pad_mask):
+        x_encoded = self.encode(x, x_pad_mask)
+        return self.decode(y, x_encoded, x_pad_mask, y_pad_mask)
+
+
+
+
+
+
+def get_transformer_encoder_backbone_bigger__v3() -> nn.TransformerEncoder:
+    d_model = 128
+    num_encoder_layers = 4
+    num_heads_encoder = 4
+    dim_feedforward = 128
+    dropout = 0.1
+    activation = F.relu
+
+    encoder_norm = nn.LayerNorm(d_model, eps=1e-5, bias=True)
+
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model=d_model,
+        nhead=num_heads_encoder,
+        dim_feedforward=dim_feedforward,
+        dropout=dropout,
+        activation=activation,
+    )
+    
+    encoder = nn.TransformerEncoder(
+        encoder_layer,
+        num_layers=num_encoder_layers,
+        norm=encoder_norm,
+    )
+
+    return encoder
+
+
+def get_transformer_decoder_backbone_bigger__v3() -> nn.TransformerDecoder:
+    d_model = 128
+    num_decoder_layers = 4
+    num_heads_decoder = 4
+    dim_feedforward = 128
+    dropout = 0.1
+    activation = F.relu
+
+    decoder_norm = nn.LayerNorm(d_model, eps=1e-5, bias=True)
+
+    decoder_layer = nn.TransformerDecoderLayer(
+        d_model=d_model,
+        nhead=num_heads_decoder,
+        dim_feedforward=dim_feedforward,
+        dropout=dropout,
+        activation=activation,
+    )
+    
+    decoder = nn.TransformerDecoder(
+        decoder_layer,
+        num_layers=num_decoder_layers,
+        norm=decoder_norm,
+    )
+
+    return decoder
+                                                
+
+
+def get_word_char_embedding_model_bigger__v3(d_model: int, n_word_chars: int, 
+                                             max_out_seq_len: int=35, 
+                                             dropout: float=0.1,
+                                             device=None) -> nn.Module:
+    word_char_embedding = nn.Embedding(n_word_chars, d_model)
+    dropout = 0.1
+    word_char_emb_dropout = nn.Dropout(dropout)
+    word_char_pos_encoder = PositionalEncoding(d_model, max_out_seq_len, device=device)
+
+    word_char_embedding_model = nn.Sequential(
+        word_char_embedding,
+        word_char_emb_dropout,
+        word_char_pos_encoder
+    )
+
+    return word_char_embedding_model
+
+
+
+def _get_transformer_bigger__v3(input_embedding: nn.Module,
+                                device = None):
+    CHAR_VOCAB_SIZE = 37  # = len(word_char_tokenizer.char_to_idx)
+    MAX_CURVES_SEQ_LEN = 299
+    MAX_OUT_SEQ_LEN = 35  # word_char_tokenizer.max_word_len - 1
+
+    n_word_chars = CHAR_VOCAB_SIZE
+
+
+    n_classes = CHAR_VOCAB_SIZE - 2  # <sos> and <pad> are not predicted
+
+
+    key_emb_size = 122
+    n_coord_feats = 6
+    d_model = n_coord_feats + key_emb_size
+
+    device = torch.device(
+        device
+        or 'cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    word_char_embedding_model = get_word_char_embedding_model_bigger__v3(
+        d_model, n_word_chars, max_out_seq_len=MAX_OUT_SEQ_LEN,
+        dropout=0.1, device=device)
+
+
+    out = nn.Linear(d_model, n_classes, device = device)
+
+
+    encoder = get_transformer_encoder_backbone_bigger__v3()
+    decoder = get_transformer_decoder_backbone_bigger__v3()
+
+
+    return partial(
+        EncoderDecoderTransformerLike,
+        input_embedding, word_char_embedding_model, encoder, decoder, out
+    )
+
+
+
+
+def get_transformer_bigger_weighted__v3(device = None, 
+                                        weights_path = None):
+    CHAR_VOCAB_SIZE = 37  # = len(word_char_tokenizer.char_to_idx)
+    MAX_CURVES_SEQ_LEN = 299
+    # Actually, n_keys != n_word_chars. n_keys = 36.
+    # It's legacy. It should not affect the model performace though.
+    n_keys = CHAR_VOCAB_SIZE
+
+    key_emb_size = 122
+    # n_coord_feats = 6
+    # d_model = n_coord_feats + key_emb_size
+
+    device = torch.device(
+        device 
+        or 'cuda' if torch.cuda.is_available() else 'cpu')
+
+    input_embedding = SeparateTrajAndWEightedEmbeddingWithPos(
+        n_keys=n_keys, key_emb_size=key_emb_size, 
+        max_len=MAX_CURVES_SEQ_LEN, device = device, dropout=0.1)
+    
+    model = _get_transformer_bigger__v3(input_embedding, device)
+
+    if weights_path:
+        model.load_state_dict(
+            torch.load(weights_path, map_location = device))
+
+    model = model.to(device)
+    model = model.eval()
+
+    return model
+
+
+
+
+def get_transformer_bigger_nearest__v3(device = None,
+                                       weights_path = None):
+    device = torch.device(
+        device 
+        or 'cuda' if torch.cuda.is_available() else 'cpu')
+
+    input_embedding = SeparateTrajAndNearestEmbeddingWithPos(
+        n_keys=37, key_emb_size=122, 
+        max_len=299, device = device, dropout=0.1)
+    
+    model = _get_transformer_bigger__v3(input_embedding, device)
+
+    if weights_path:
+        model.load_state_dict(
+            torch.load(weights_path, map_location = device))
+
+    model = model.to(device)
+    model = model.eval()
+
+    return model
+
+
+
+
+
 
 MODEL_GETTERS_DICT = {
     "m1": get_m1_model,  # doesn't have layer norm
@@ -1408,5 +1625,9 @@ MODEL_GETTERS_DICT = {
 
 
     "v2_weighted_transformer_bigger": get_transformer_bigger_weighted__v2,  # has layer norm
-    "v2_nearest_transformer_bigger": get_transformer_bigger_nearest__v2  # has layer norm
+    "v2_nearest_transformer_bigger": get_transformer_bigger_nearest__v2,  # has layer norm
+
+
+    "v3_weighted_transformer_bigger": get_transformer_bigger_weighted__v3,  # has layer norm
+    "v3_nearest_transformer_bigger": get_transformer_bigger_nearest__v3,  # has layer norm
 }
