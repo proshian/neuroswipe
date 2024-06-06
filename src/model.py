de424bdd35eb575452c0eb7218c3c46c756290f5
Garri Proshian <proshian.itmo@gmail.com>
2024-06-06 13:19:28 +0300

    Update PositionalEncoding: Add odd d_model support

--- src/model.py
+++ src/model.py
@@ -108,10 +108,14 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         position = torch.arange(max_len).unsqueeze(1)
+        # ```d_model // 2``` elements
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        if d_model % 2:
+            pe[:, 0, 1::2] = torch.cos(position * div_term)
+        else:
+            pe[:, 0, 1::2] = torch.cos(position * div_term[:-1])
         pe = pe.to(device=device)
         self.register_buffer('pe', pe)
 
